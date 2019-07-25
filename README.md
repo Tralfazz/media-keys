eca24e97e986bdac3858877c749a61052ea14399
Tralfazz <matan.yesharim@gmail.com>
2019-07-25 22:14:10 +0300

    my fork, changed to playerctl

--- README.md
+++ README.md
@@ -2,16 +2,13 @@
 
 Gnome-shell extension that adds Media Player buttons to the panel.
 
-#### !!! NEEDS xdotool
+## Notes
 
-### Changelog
+* The extension uses **playerctl** to control all MPRIS audio streams so it needs to be installed
+* **mpris-proxy** also needs to run first
+* work in progress, need to find out how to get the current state of the player
+* this is a fork of https://github.com/mafflin/media-keys
 
-* v.5
-  - Works with X11 only!
-
-### Notes
-
-* Extension emulates buttons:
-  - XF86AudioPlay
-  - XF86AudioPrev
-  - XF86AudioNext
+## dependencies
+* mpris-proxy
+* playerctl
--- extension.js
+++ extension.js
@@ -9,37 +9,55 @@ const iconPrev = new St.Icon({
   icon_name: 'media-skip-backward-symbolic',
   style_class: 'system-status-icon'
 });
+
+const iconPlay = new St.Icon({
+  icon_name: 'media-playback-start-symbolic',
+  style_class: 'system-status-icon'
+});
+
 const iconPause = new St.Icon({
   icon_name: 'media-playback-pause-symbolic',
   style_class: 'system-status-icon'
 });
+
+const PlayPause = [iconPause, iconPlay]
+
 const iconNext = new St.Icon({
   icon_name: 'media-skip-forward-symbolic',
   style_class: 'system-status-icon'
 });
 
 let buttonPrev = new St.Button({ style_class: 'panel-button'});
-let buttonPause = new St.Button({ style_class: 'panel-button'});
+let buttonPlayPause = new St.Button({ style_class: 'panel-button'});
 let buttonNext = new St.Button({ style_class: 'panel-button'});
 
-function _pause() {
-  Util.spawn(['xdotool', 'key', 'XF86AudioPlay']);
+//keep track of the toggle
+let isPlaying = 0; // add spawn async
+
+function set_play_icon(button) {
+  button.set_child(PlayPause[isPlaying])
+  isPlaying ^= 1 // toggle
+}
+
+function _play_pause(button) {
+  set_play_icon(button)
+  Util.spawn(['playerctl' ,'play-pause']);
 }
 
 function _prev() {
-  Util.spawn(['xdotool', 'key', 'XF86AudioPrev']);
+  Util.spawn(['playerctl' ,'previous']);
 }
 
 function _next() {
-  Util.spawn(['xdotool', 'key', 'XF86AudioNext']);
+  Util.spawn(['playerctl','next']);
 }
 
 function init() {
   buttonPrev.set_child(iconPrev);
   buttonPrev.connect('button-press-event', _prev);
 
-  buttonPause.set_child(iconPause);
-  buttonPause.connect('button-press-event', _pause);
+  set_play_icon(buttonPlayPause)
+  buttonPlayPause.connect('button-press-event', _play_pause);
 
   buttonNext.set_child(iconNext);
   buttonNext.connect('button-press-event', _next);
@@ -47,12 +65,12 @@ function init() {
 
 function enable() {
   Main.panel._rightBox.insert_child_at_index(buttonNext, 2);
-  Main.panel._rightBox.insert_child_at_index(buttonPause, 2);
+  Main.panel._rightBox.insert_child_at_index(buttonPlayPause, 2);
   Main.panel._rightBox.insert_child_at_index(buttonPrev, 2);
 }
 
 function disable() {
   Main.panel._rightBox.remove_child(buttonPrev);
-  Main.panel._rightBox.remove_child(buttonPause);
+  Main.panel._rightBox.remove_child(buttonPlayPause);
   Main.panel._rightBox.remove_child(buttonNext);
 }
